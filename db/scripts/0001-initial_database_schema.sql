2663289d3eface5fbbb1d87d1620111631b35e15
osman perviz <osmanperviz@gmail.com>
2019-05-08 23:30:27 +0200

    Load events, update user mutation.

--- db/scripts/0001-initial_database_schema.sql
+++ db/scripts/0001-initial_database_schema.sql
@@ -12,5 +12,20 @@ CREATE TABLE user_read_model (
   id varchar(40),
   read_model jsonb NOT NULL,
   schema_version integer,
+  text_search tsvector,
   CONSTRAINT user_read_model_primary_key PRIMARY KEY(id)
 );
+
+CREATE FUNCTION update_user_read_model_text_search() RETURNS trigger AS $$
+  BEGIN
+    new.text_search :=
+      setweight(to_tsvector('german', coalesce(new.read_model->>'email','')), 'A') ||
+      setweight(to_tsvector('german', coalesce(new.read_model->>'firstName','')), 'A') ||
+      setweight(to_tsvector('german', coalesce(new.read_model->>'lastName','')), 'A');
+    RETURN new;
+  END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER user_read_model_text_search_trigger
+  BEFORE INSERT OR UPDATE ON user_read_model FOR EACH ROW
+  EXECUTE PROCEDURE update_user_read_model_text_search();
